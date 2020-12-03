b91a4042719f784d5215a57f708df94ae44db3ea
jhonata rodrigues <jhonata.a.r@hotmail.com>
2020-12-02 21:59:11 -0300

    porta https

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import app from './app'
 import './config'
 import moment from 'moment-timezone'
+import http from 'http'
+import https from 'https'
 
-const port = process.env.PORT || 8080
+const portHttp = 8080
+const portHttps = 8085
 
 // -- config moment locale
 moment.tz.setDefault('America/Sao_Paulo')
 moment.locale('pt-br')
 
-console.log(`server on port ${port}`)
+console.log(`server on port ${portHttp} and https: ${portHttps}`)
 
-app.listen(port)
+const httpServer = http.createServer(app)
+const httpsServer = https.createServer(app)
+
+httpServer.listen(portHttp)
+httpsServer.listen(portHttps)
