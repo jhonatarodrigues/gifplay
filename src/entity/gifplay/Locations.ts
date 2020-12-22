a712ab8a0bc085a1e34c5aed3ac513094e656803
jhonata rodrigues <jhonata.a.r@hotmail.com>
2020-12-22 08:32:39 -0300

    campo public

--- src/entity/gifplay/Locations.ts
+++ src/entity/gifplay/Locations.ts
@@ -3,26 +3,29 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 @Entity('locations', { schema: 'gifplay' })
 export class Locations {
   @PrimaryGeneratedColumn({ type: 'int', name: 'id', unsigned: true })
-  id: number;
+  id: number
 
   @Column('int', { name: 'space_id' })
-  spaceId: number | null;
+  spaceId: number | null
 
   @Column('varchar', { name: 'player_email', nullable: true, length: 200 })
-  playerEmail: string | null;
+  playerEmail: string | null
 
   @Column('datetime', { name: 'time_start', nullable: true })
-  timeStart: Date | null;
+  timeStart: Date | null
 
   @Column('datetime', { name: 'time_end', nullable: true })
-  timeEnd: Date | null;
+  timeEnd: Date | null
 
   @Column('datetime', { name: 'register_date', nullable: true })
-  registerDate: Date | null;
+  registerDate: Date | null
+
+  @Column('varchar', { name: 'public', nullable: true, length: 50 })
+  public: string | null
 
   @Column('varchar', { name: 'status', nullable: true, length: 250 })
-  status: string | null;
+  status: string | null
 
   @Column('varchar', { name: 'erro', nullable: true, length: 250 })
-  erro: string | null;
+  erro: string | null
 }
